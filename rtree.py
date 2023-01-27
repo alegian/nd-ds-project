c7b464c3407f58ce9de0d955f97316dc2daddfe3
Alexandros Karagiannis <alegiannx@gmail.com>
2023-01-27 19:07:43 +0200

    fix rtree range query

--- rtree.py
+++ rtree.py
@@ -11,6 +11,18 @@ class MBR:
     def contains(self, x, y):
         return self.xmin <= x <= self.xmax and self.ymin <= y <= self.ymax
 
+    def intersects(self, mbr):
+        self_contains_mbr_x = self.xmin <= mbr.xmin <= self.xmax or self.xmin <= mbr.xmax <= self.xmax
+        mbr_contains_self_x = mbr.xmin <= self.xmin <= mbr.xmax or mbr.xmin <= self.xmax <= mbr.xmax
+        intersects_x = self_contains_mbr_x or mbr_contains_self_x
+        if intersects_x:
+            self_contains_mbr_y = self.ymin <= mbr.ymin <= self.ymax or self.ymin <= mbr.ymax <= self.ymax
+            mbr_contains_self_y = mbr.ymin <= self.ymin <= mbr.ymax or mbr.ymin <= self.ymax <= mbr.ymax
+            intersects_y = self_contains_mbr_y or mbr_contains_self_y
+            return intersects_y
+        else:
+            return False
+
     def extend(self, x, y):
         new_mbr = MBR(self.xmin, self.xmax, self.ymin, self.ymax)
         if new_mbr.xmin > x:
@@ -78,6 +90,7 @@ class RTree:
                 min_child.children.append(new_node)
 
     def range_query(self, surname_min, surname_max, award_min, award_max):
+        query_mbr = MBR(surname_min, surname_max, award_min, award_max)
         if self.is_leaf():
             if surname_min <= self.data.surname <= surname_max and award_min <= self.data.awards <= award_max:
                 return [self.data]
@@ -86,7 +99,7 @@ class RTree:
         else:
             out = []
             for child in self.children:
-                if child is not None:
+                if child is not None and query_mbr.intersects(child.mbr):
                     out.extend(child.range_query(surname_min, surname_max, award_min, award_max))
             return list(set(out))
 
